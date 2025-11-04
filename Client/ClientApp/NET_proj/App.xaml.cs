44bd829beef1e5ec55fbb2fc8f23d734ec0c1267
inna.savielieva1 <inna.savielieva1@gmail.com>
2025-11-05 00:30:41 +0200

    Initial MAUI project setup.

---
+++ Client/ClientApp/NET_proj/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NET_proj
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Client/ClientApp/NET_proj/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NET_proj
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
